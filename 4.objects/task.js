376085c3b129083e5359d2023632506a7202163b
Elias Leushin <Fajrolution@gmail.com>
2024-12-18 13:32:36 +0500

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = []; // Свойство для хранения оценок
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+    this.subject = subjectName; // Устанавливаем значение предмета
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.marks) { // Проверяем, существует ли массив оценок
+        this.marks.push(...marksToAdd); // Добавляем оценки в массив
+    }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+    if (!this.marks || this.marks.length === 0) {
+        return 0; // Возвращаем 0, если оценок нет
+    }
+    
+    const sum = this.marks.reduce((acc, mark) => acc + mark, 0); // Суммируем оценки
+    return sum / this.marks.length; // Возвращаем среднее
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+    delete this.subject; // Удаляем предмет
+    delete this.marks; // Удаляем оценки
+    this.excluded = reason; // Устанавливаем причину исключения
+};
